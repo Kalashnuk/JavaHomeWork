b1a6f1fdf2202b1b89a4c57b3467ac0babd56404
Kalashnyk Oleh <fibibiza@gmail.com>
2021-11-07 23:31:47 +0200

    init commit

--- src/com/pb/kalashnyk/hw6/Veterinarian.java
+++ src/com/pb/kalashnyk/hw6/Veterinarian.java
@@ -3,6 +3,6 @@ package com.pb.kalashnyk.hw6;
 public class Veterinarian {
     public void treatAnimal(Animal animal) {
         System.out.println("At the doctor's appointment: "+animal.getName()+
-                " loves to eat "+ animal.getFood()+" lives "+ animal.getLocation());
+                " loves to eat "+ animal.getFood()+" lives "+ animal.getLocation()+".");
     }
 }
